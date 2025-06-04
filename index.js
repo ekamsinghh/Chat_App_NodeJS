1e6452aeff122e84444e29ee8abfc82fb80d255a
Ekam Singh <ekamsinghh24@gmail.com>
2025-06-05 02:12:55 +0530

    Added chat logic

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const app=express();
 
+const Chat=require('./models/chat');
+
+const connect = require('./config/database-config');
+
 const http = require('http');
 const server = http.createServer(app);
 
@@ -10,19 +14,50 @@ const io = new socketio.Server(server);
 app.use('/',express.static(__dirname+'/public'));
 //* it helps to connect to static files like HTML
 
+app.set('view engine','ejs');//* it helps to connect to ejs files
+
+
+app.get('/chat/:roomid',async (req,res)=>{
+    const chats=await Chat.find({
+        roomId: req.params.roomid
+    });
+    console.log("chats: ",chats);
+    res.render('index',{
+        message: "Connected to your personalised chat room",
+        id: req.params.roomid,
+        chats: chats
+    });
+});
+
+
+
 io.on('connection',(socket) => {
     console.log("A user connected");
 
+    socket.on("join_room",(data)=>{
+        console.log("Recieved data: ",data);
+        socket.join(data.roomid);
+    });
 
-    socket.on("btn_click",(data)=>{
+
+    socket.on("btn_click",async (data)=>{
+        // io.emit("data recieved",data.msg);
+        //* "io.emit" will emit it to the every websocket connection on the server
+        //* whereas "socket.emit" will emit it to the specific socket that is connected to the server
+        //* "socket.broadcast.emit" will emit it to all the sockets except the one that is connected
+
         console.log(data);
-        io.emit("data recieved",data);
-        //* io.emit will emit it to the every websocket connection on the server
-        //* whereas socket.emit will emit it to the specific socket that is connected to the server
-        //* socket.broadcast.emit will emit it to all the sockets except the one that is connected
-    })
+        const chat=await Chat.create({
+            roomId: data.roomid,
+            content: data.msg,
+            user: data.username
+        });
+        io.to(data.roomid).emit("data recieved",data);
+    });
 });
 
-server.listen(3000,()=>{
+server.listen(3000,async ()=>{
     console.log("Server started");
+    await connect();
+    console.log("MongoDB connected");
 });
